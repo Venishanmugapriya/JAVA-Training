0728acea8c0c1802c45e57d258e7c0a7100c3b96
Venishanmugapriya <155565716+Venishanmugapriya@users.noreply.github.com>
2024-01-04 00:17:53 +0530

    Add files via upload

---
+++ VotingAge/VotingAge.java
@@ -0,0 +1,13 @@
+class VotingAge{
+public static void main(String args[]){
+final int votingAge=18;
+int userAge=21;
+if(userAge>=votingAge)
+{
+System.out.print("Eligible");
+}
+else{
+System.out.print("Not Eligible");
+}
+}
+}
