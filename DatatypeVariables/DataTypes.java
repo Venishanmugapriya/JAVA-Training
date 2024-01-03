2ff7831fdb358c46f7f593e72f665f817c02507e
Venishanmugapriya <155565716+Venishanmugapriya@users.noreply.github.com>
2024-01-03 22:44:39 +0530

    Add files via upload

---
+++ DatatypeVariables/DataTypes.java
@@ -0,0 +1,22 @@
+class DataTypes{
+public static void main(String args[])
+{
+boolean bool = true;
+float f=0.2f;
+double d=150.5d;
+char c='#';
+byte b=5;
+short s=8;
+int i=1;
+long l=1l;
+String str="Sitaara";
+System.out.println(bool);
+System.out.println(f);
+System.out.println(d);
+System.out.println(c);
+System.out.println(b);
+System.out.println(s);
+System.out.println(i);
+System.out.println(l);
+}
+}
