c5ebc37dbdac1fa6bbe7beadc984dff7b6946aed
Venishanmugapriya <155565716+Venishanmugapriya@users.noreply.github.com>
2024-01-04 22:23:37 +0530

    Add files via upload

---
+++ VariableArray/VariableArray.java
@@ -0,0 +1,66 @@
+class VariableArray{
+public static void main(String[] args)
+{
+boolean var[]=new boolean[2];
+var[0]=true;
+var[1]=false;
+boolean vars[]=new boolean[]{false,true};
+char character[]=new char[2];
+character[0]='a';
+character[1]='@';
+char charact[]=new char[]{'b','5'};
+byte num[]=new byte[2];
+num[0]=2;
+num[1]=5;
+byte nums[]=new byte[]{7,4};
+short even[]=new short[2];
+even[0]=2;
+even[1]=8;
+short odd[]=new short[]{1,3};
+int age[]=new int[2];
+age[0]=20;
+age[1]=35;
+int ages[]=new int[]{25,22};
+long number[]=new long[2];
+number[0]=23456l;
+number[1]=875643l;
+float price[]=new float[2];
+price[0]=1.5f;
+price[1]=2.5f;
+float prices[]=new float[]{8.7f,4.3f};
+double discount[]=new double[2];
+discount[0]=1000.75d;
+discount[1]=8543.75d;
+double discounts[]=new double[]{3456.875d,9876.475d};
+System.out.println(var[0]);
+System.out.println(var[1]);
+System.out.println(vars[1]);
+System.out.println(vars[0]);
+System.out.println(character[1]);
+System.out.println(character[0]);
+System.out.println(charact[1]);
+System.out.println(charact[0]);
+System.out.println(num[1]);
+System.out.println(num[0]);
+System.out.println(nums[0]);
+System.out.println(nums[1]);
+System.out.println(even[1]);
+System.out.println(even[0]);
+System.out.println(odd[0]);
+System.out.println(odd[1]);
+System.out.println(age[0]);
+System.out.println(age[1]);
+System.out.println(ages[1]);
+System.out.println(ages[0]);
+System.out.println(number[0]);
+System.out.println(number[1]);
+System.out.println(price[0]);
+System.out.println(price[1]);
+System.out.println(prices[1]);
+System.out.println(prices[0]);
+System.out.println(discount[0]);
+System.out.println(discount[1]);
+System.out.println(discounts[0]);
+System.out.println(discounts[1]);
+}
+}
