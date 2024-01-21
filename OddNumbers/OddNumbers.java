e7188b1bf2a2497516e4b7d46a98fa93bc4dd4c0
Venishanmugapriya <155565716+Venishanmugapriya@users.noreply.github.com>
2024-01-21 16:03:54 +0530

    Add files via upload

---
+++ OddNumbers/OddNumbers.java
@@ -0,0 +1,15 @@
+class OddNumbers{
+ public static void main(String[] args) {
+ int sum=0;
+ for(int i=51;i<100;i++)
+{
+if(i%2==1)
+{
+sum=sum+i;
+}
+}
+System.out.println(sum);
+
+         
+    }
+}
