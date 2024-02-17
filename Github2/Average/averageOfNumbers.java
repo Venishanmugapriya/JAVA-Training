6e830594992407ac0545b295578c6b67469ec665
Venishanmugapriya <155565716+Venishanmugapriya@users.noreply.github.com>
2024-02-17 12:40:46 +0530

    Add files via upload

---
+++ Github2/Average/averageOfNumbers.java
@@ -0,0 +1,9 @@
+public class averageOfNumbers{
+public static void main(String[]args){
+int num1=15;
+int num2=10;
+double average=(num1+num2)/2;
+System.out.println("The average is: "+average);
+}
+}
+
