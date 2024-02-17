6e40dd094845e4236539afd5fa6c1b41124ca2e0
Venishanmugapriya <155565716+Venishanmugapriya@users.noreply.github.com>
2024-02-17 21:46:07 +0530

    Add files via upload

---
+++ Github2/Area of the rectangle/areaOfRectangle.java
@@ -0,0 +1,8 @@
+public class areaOfRectangle{
+public static void main(String[]args){
+int length=15;
+int width=10;
+int area=length*width;
+System.out.println("The area of the rectangle is: "+area);
+}
+}
