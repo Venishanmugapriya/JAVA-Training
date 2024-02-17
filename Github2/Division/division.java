24d99482d0f37cf9e88d65b64cfc239d873b6acd
Venishanmugapriya <155565716+Venishanmugapriya@users.noreply.github.com>
2024-02-17 12:05:06 +0530

    Add files via upload

---
+++ Github2/Division/division.java
@@ -0,0 +1,8 @@
+public class division{
+public static void main(String[]args){
+int dividend=35;
+int divisor=4;
+int reminder=dividend%divisor;
+System.out.println("The reminder is: "+reminder);
+}
+}
