4a84c7dba641aa850140a95904cda1bd8e0479b7
Venishanmugapriya <155565716+Venishanmugapriya@users.noreply.github.com>
2024-02-17 21:38:40 +0530

    Add files via upload

---
+++ Github2/Even or Odd/evenOrOdd.java
@@ -0,0 +1,13 @@
+public class evenOrOdd{
+public static void main(String[] args) {
+int number = 15;
+if (number % 2 == 0)
+{
+System.out.println(number + " is even.");
+}
+else
+{
+System.out.println(number + " is odd.");
+}
+}
+}
