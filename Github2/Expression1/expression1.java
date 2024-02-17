d119abd4c2ddfd699717d16ec05ed4b78faec917
Venishanmugapriya <155565716+Venishanmugapriya@users.noreply.github.com>
2024-02-17 12:24:57 +0530

    Add files via upload

---
+++ Github2/Expression1/expression1.java
@@ -0,0 +1,10 @@
+public class expression1{
+public static void main(String[]args){
+int a=8;
+int b=3;
+int c=2;
+int d=5;
+int result=(a-b)*c/d;
+System.out.println("The result is: "+result);
+}
+}
