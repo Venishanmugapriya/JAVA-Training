7576bda255d8871135389911f16b2774e75b8bef
Venishanmugapriya <155565716+Venishanmugapriya@users.noreply.github.com>
2024-02-19 22:54:30 +0530

    Add files via upload

---
+++ Github3(Control Statements)/Maximum Number/maxNum.java
@@ -0,0 +1,17 @@
+public class maxNum{
+public static void main(String[]args){
+int a=8,b=5,c=40;
+if(a>b && a>c)
+{
+System.out.println("a is big");
+}
+else if(b>a && b>c)
+{
+System.out.println("b is big");
+}
+else
+{
+System.out.println("c is big");
+}
+}
+}
