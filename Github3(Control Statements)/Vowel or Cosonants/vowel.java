c57a8f893de29492f7dd2c333900c81c5e44b21d
Venishanmugapriya <155565716+Venishanmugapriya@users.noreply.github.com>
2024-02-19 22:59:38 +0530

    Add files via upload

---
+++ Github3(Control Statements)/Vowel or Cosonants/vowel.java
@@ -0,0 +1,29 @@
+public class vowel{
+public static void main(String[]args){
+char ch='V';
+if(ch=='A')
+{
+System.out.println(ch + " is vowel");
+}
+else if(ch=='E')
+{
+System.out.println(ch + " is vowel");
+}
+else if(ch=='I')
+{
+System.out.println(ch + " is vowel");
+}
+else if(ch=='O')
+{
+System.out.println(ch + " is vowel");
+}
+else if(ch=='U')
+{
+System.out.println(ch + " is vowel");
+}
+else
+{
+System.out.println(ch + " is consonant");
+}
+}
+}
