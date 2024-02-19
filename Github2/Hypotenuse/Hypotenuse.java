5daad312a5c520bb4d6b5c4e3c6a7ddffac03ea1
Venishanmugapriya <155565716+Venishanmugapriya@users.noreply.github.com>
2024-02-19 22:21:18 +0530

    Add files via upload

---
+++ Github2/Hypotenuse/Hypotenuse.java
@@ -0,0 +1,8 @@
+public class Hypotenuse{
+public static void main(String[]args){
+int a=15;
+int b=10;
+double hypotenuse=Math.sqrt(a*a+b*b);
+System.out.println("The Hypotenuse of a right triangle "+hypotenuse);
+}
+}
