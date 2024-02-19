ae1f308df8ab4de0a68321907dd87ab24dcbc796
Venishanmugapriya <155565716+Venishanmugapriya@users.noreply.github.com>
2024-02-19 23:23:58 +0530

    Add files via upload

---
+++ Github3(Control Statements)/Num Divisible by 2&3/divisibleby23.java
@@ -0,0 +1,9 @@
+public class divisibleby23{
+public static void main(String[]args){
+int  number=18;
+if(number%2==0 && number%3==0)
+System.out.println(number+" is divisible by both 2 and 3");
+else
+System.out.println(number+" is not divisible by both 2 and 3");
+}
+}
