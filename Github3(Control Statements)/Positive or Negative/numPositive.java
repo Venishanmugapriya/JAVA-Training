627b8e9ffe06b8323c4c1e9a06e369c4ce8fb4af
Venishanmugapriya <155565716+Venishanmugapriya@users.noreply.github.com>
2024-02-19 22:33:01 +0530

    Add files via upload

---
+++ Github3(Control Statements)/Positive or Negative/numPositive.java
@@ -0,0 +1,17 @@
+public class numPositive{
+public static void main(String[]args){
+int num=-3;
+if(num>0)
+{
+System.out.println(num+" is positive");
+}
+else if(num==0)
+{
+System.out.println("Number is zero");
+}
+else
+{
+System.out.println(num+" is negative");
+}
+}
+}
