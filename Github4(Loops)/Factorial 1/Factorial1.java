bd558451abd98b02fd9d7c20a13d1275d8dcaeed
Venishanmugapriya <155565716+Venishanmugapriya@users.noreply.github.com>
2024-02-22 20:06:21 +0530

    Add files via upload

---
+++ Github4(Loops)/Factorial 1/Factorial1.java
@@ -0,0 +1,10 @@
+class Factorial1{
+public static void main(String[]args){
+int i,fact=1;
+int number=4;
+for(i=1;i<=number;i++){
+fact=fact*i;
+}
+System.out.print("Factorial of a given number is:"+fact);
+}
+}
