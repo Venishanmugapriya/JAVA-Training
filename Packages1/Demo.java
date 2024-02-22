7fa44a0cdaed7dac8d241cd8a115026a9b05143e
Venishanmugapriya <155565716+Venishanmugapriya@users.noreply.github.com>
2024-02-22 18:49:56 +0530

    Add files via upload

---
+++ Packages1/Demo.java
@@ -0,0 +1,9 @@
+package com.besant.trackerold;
+public class Demo{
+public static void testAccess(){
+System.out.println("Public Method");
+}
+protected static void printHello(){
+System.out.println("Hello World");
+}
+}
---
+++ Packages1/Main.java
@@ -0,0 +1,8 @@
+package com.besant.trackernew;
+import com.besant.trackerold.Demo;
+public class Main extends Demo{
+public static void main(String[]args){
+new Demo().testAccess();
+new Demo().printHello();
+}
+}
