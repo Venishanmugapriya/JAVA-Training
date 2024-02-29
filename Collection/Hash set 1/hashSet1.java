590c321380dbeb19e2f7c242673dd86a104b94eb
Venishanmugapriya <155565716+Venishanmugapriya@users.noreply.github.com>
2024-02-29 09:42:31 +0530

    Add files via upload

---
+++ Collection/Hash set 1/hashSet1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class hashSet1{
+public static void main(String[]args){
+Set<String> namelist=new HashSet<>();
+namelist.add("Aadya");
+namelist.add("Veni");
+namelist.add("Taara");
+namelist.add("Sitaara");
+namelist.add("Veni");
+namelist.add("Aanvi");
+namelist.add("Advika");
+namelist.add("Shaurya");
+System.out.println(namelist);
+System.out.println(namelist.size());
+}
+}
