1d6a2439c3265f965b143557656c31317d98f976
Venishanmugapriya <155565716+Venishanmugapriya@users.noreply.github.com>
2024-02-29 09:43:00 +0530

    Add files via upload

---
+++ Collection/Linked List1/linkedListEg.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class linkedListEg{
+public static void main(String[]args){
+LinkedList<String> arr=new LinkedList<>();
+arr.add("Hi");
+arr.add("Subscribe");
+arr.add("Comment");
+arr.add("Hi");
+arr.add("Subscribe");
+arr.add("Comment");
+System.out.println(arr);
+System.out.println(arr.size());
+}
+}
