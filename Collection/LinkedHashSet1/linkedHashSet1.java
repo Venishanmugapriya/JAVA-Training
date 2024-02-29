7cccfddf52bf0fcb51970b8c7d08d8eb026d7796
Venishanmugapriya <155565716+Venishanmugapriya@users.noreply.github.com>
2024-02-29 09:50:56 +0530

    Add files via upload

---
+++ Collection/LinkedHashSet1/linkedHashSet1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class linkedHashSet1{
+public static void main(String[]args){
+Set<String> nameList=new LinkedHashSet<String>();
+nameList.add("Advika");
+nameList.add("Veni");
+nameList.add("Priya");
+nameList.add("Taara");
+nameList.add("Sitaara");
+nameList.add("Aadya");
+nameList.add("Aanvi");
+nameList.add("Sitaara");
+System.out.println(nameList);
+}
+}
