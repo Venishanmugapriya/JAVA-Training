b12b40082ebfec44f22c4736e82f92338b3f7312
Venishanmugapriya <155565716+Venishanmugapriya@users.noreply.github.com>
2024-03-18 19:05:28 +0530

    Add files via upload

---
+++ Java Pattern Programs/Pattern3/Pattern3.java
@@ -0,0 +1,11 @@
+public class Pattern3{
+public static void main(String[]args){
+int n=4;
+for(int i=1;i<=n-1;i++){
+System.out.println("*");
+}
+for(int i=1;i<=n;i++){
+System.out.print("* ");           
+}
+}
+}
