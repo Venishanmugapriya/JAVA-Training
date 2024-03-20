9ac2174d44cc0a5bc91476f50820a61d59a6e3ee
Venishanmugapriya <155565716+Venishanmugapriya@users.noreply.github.com>
2024-03-20 18:50:19 +0530

    Add files via upload

---
+++ Java Pattern Programs/Pattern12/Pattern12.java
@@ -0,0 +1,13 @@
+public class Pattern12{
+public static void main(String[]args){
+for(int i=1;i<=7;i++){
+for(int k=7;k>=i;k--){
+System.out.print(" ");
+}
+for(int j=1;j<=i;j++){
+System.out.print(j+" ");
+}
+System.out.println();
+}
+} 
+}
