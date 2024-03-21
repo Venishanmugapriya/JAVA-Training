a201d65f16570c8c66f770cbd7d23976b3e2678c
Venishanmugapriya <155565716+Venishanmugapriya@users.noreply.github.com>
2024-03-21 23:44:50 +0530

    Add files via upload

---
+++ Java Pattern Programs/Pattern29/Pattern29.java
@@ -0,0 +1,23 @@
+public class Pattern29{
+public static void main(String[]args){
+for(int i=7;i>=1;i--){
+for(int k=7;k>=i;k--){
+System.out.print(" ");
+}
+for(int j=1;j<=i;j++){
+System.out.print("*"+" ");
+}
+System.out.println();
+}
+for(int l=2;l<=7;l++){
+for(int m=7;m>=l;m--){
+System.out.print(" ");
+}
+for(int n=1;n<=l;n++){
+System.out.print("*"+" ");
+}
+
+System.out.println();
+}
+} 
+}
