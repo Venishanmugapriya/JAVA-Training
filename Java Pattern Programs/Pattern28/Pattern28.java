a7faa53fab41247bd555df0aae25dbfd9357ea76
Venishanmugapriya <155565716+Venishanmugapriya@users.noreply.github.com>
2024-03-21 23:41:41 +0530

    Add files via upload

---
+++ Java Pattern Programs/Pattern28/Pattern28.java
@@ -0,0 +1,23 @@
+public class Pattern28{
+public static void main(String[] args) {
+int n=7;
+for(int i=n;i>=1;i--){ 
+for(int k=n;k>i;k--){
+System.out.print("  ");   
+}
+for(int j=1;j<=i;j++){
+System.out.print("*"+" ");   
+}
+System.out.println();
+}
+for(int i=2;i<=n;i++){ 
+for(int k=1;k<=n-i;k++){
+System.out.print("  ");   
+}
+for(int j=1;j<=i;j++){
+System.out.print("*"+" ");   
+}
+System.out.println();
+}
+}
+}
